b1f8487334e2119abf0d758d3d11231f79f3bfbb
eu-company <powerbear008@gmail.com>
2017-06-22 18:39:28 +0800

    react router must read doc

---
+++ src/components/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound = () => {
+	<div>
+		<h3>No match </h3>
+	</div>
+}
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -6,16 +6,32 @@ import './index.pug';
 // ==========================================================================
 import './sass/style.scss';
 
-// import js
+// import components js
 // ==========================================================================
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-// index js
+// import react plugins 
 // ==========================================================================
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import StorePicker from './components/StorePicker';
+// Index pages setting
+// ==========================================================================
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Route exact path="/" component={ StorePicker } />
+				<Route path="/store/:storeId" component={ App } />
+
+			</div>
+		</BrowserRouter>
+	)
+}
 
 
-render(<App />, document.querySelector('#main'));
+render(<Root />, document.querySelector('#main'));
